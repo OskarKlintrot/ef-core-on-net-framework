23e3962952532359d496672495769d43db85ef34
Oskar Klintrot <oskar.klintrot@gmail.com>
2019-05-21 19:58:22 +0200

    feat: Add controller

--- EfCoreDemo/EfCoreDemoWebApi/App_Start/WebApiConfig.cs
+++ EfCoreDemo/EfCoreDemoWebApi/App_Start/WebApiConfig.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.EntityFrameworkCore;
+using System.Web.Http.Dependencies;
+using System.Web.Http.Controllers;
 
 namespace EfCoreDemoWebApi
 {
@@ -13,6 +15,11 @@ namespace EfCoreDemoWebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            config.DependencyResolver = new DefaultDependencyResolver(services.BuildServiceProvider());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -26,7 +33,60 @@ namespace EfCoreDemoWebApi
 
         public static void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BloggingContext>(options => options.UseInMemoryDatabase("BloggingContext"));
+            services
+                .AddControllersAsServices()
+                .AddDbContext<BloggingContext>(options => options.UseInMemoryDatabase("BloggingContext"));
+        }
+    }
+
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+        protected IServiceProvider ServiceProvider { get; set; }
+
+        public DefaultDependencyResolver(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return ServiceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return ServiceProvider.GetServices(serviceType);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new DefaultDependencyResolver(ServiceProvider.CreateScope().ServiceProvider);
+        }
+
+        public void Dispose()
+        {
+            // you can implement this interface just when you use .net core 2.0
+            // ServiceProvider.Dispose();
+        }
+    }
+
+    public static class ServiceProviderExtensions
+    {
+        public static IServiceCollection AddControllersAsServices(this IServiceCollection services)
+        {
+            var controllerTypes = typeof(WebApiConfig)
+                .Assembly
+                .GetExportedTypes()
+                .Where(t => !t.IsAbstract && !t.IsGenericTypeDefinition)
+                .Where(t => typeof(IHttpController).IsAssignableFrom(t)
+                            || t.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var type in controllerTypes)
+            {
+                services.AddTransient(type);
+            }
+
+            return services;
         }
     }
 }
